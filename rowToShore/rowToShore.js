ddef8dbf996e916b1bc19a6dc8a638422a62c42b
e0425705 <e0425705@u.nus.edu>
2022-05-15 03:44:24 +0800

    need help debugging HAHA

---
+++ rowToShore/rowToShore.js
@@ -0,0 +1,224 @@
+/* 
+ * Rowing to shore (in the smaller boat)
+ * Avoiding rocks and other obstacles
+ */
+
+var score = 0;
+var lives = 3;
+
+var ship  = document.getElementById("ship");
+var stone0 = document.getElementById("stone0");
+var stone1 = document.getElementById("stone1");
+var stone2 = document.getElementById("stone2");
+var stone3 = document.getElementById("stone3");
+var stone4 = document.getElementById("stone4");
+var stone5 = document.getElementById("stone5");
+var stone6 = document.getElementById("stone6");
+var stone7 = document.getElementById("stone7");
+var stone8 = document.getElementById("stone8");
+
+var shipTop = parseInt(window.getComputedStyle(ship).getPropertyValue("top"));
+var stone0Left = parseInt(window.getComputedStyle(stone0).getPropertyValue("left"));
+var stone1Left = parseInt(window.getComputedStyle(stone1).getPropertyValue("left"));
+var stone2Left = parseInt(window.getComputedStyle(stone2).getPropertyValue("left"));
+var stone3Left = parseInt(window.getComputedStyle(stone3).getPropertyValue("left"));
+var stone4Left = parseInt(window.getComputedStyle(stone4).getPropertyValue("left"));
+var stone5Left = parseInt(window.getComputedStyle(stone5).getPropertyValue("left"));
+var stone6Left = parseInt(window.getComputedStyle(stone6).getPropertyValue("left"));
+var stone7Left = parseInt(window.getComputedStyle(stone7).getPropertyValue("left"));
+var stone8Left = parseInt(window.getComputedStyle(stone8).getPropertyValue("left"));
+
+var runStone0 = 1;
+var runStone1 = 0;
+var runStone2 = 0;
+var runStone3 = 0;
+var runStone4 = 0;
+var runStone5 = 0;
+var runStone6 = 0;
+var runStone7 = 0;
+var runStone8 = 0;
+
+function checkLives() {
+    if (lives < 1) {
+        // end game
+        alert("Game Over! Score: " + score + " Lives: " + lives);
+    }
+}
+
+function checkPosition() {
+    if (runStone0 == 1 && shipTop <= 80 && stone0Left <= 60 && stone0Left >= -80) {
+        lives--;
+    } else if (runStone1 == 1 && shipTop >= 10 && shipTop <= 150 && stone1Left <= 60 && stone1Left >= -80) {
+        lives--;
+    } else if (runStone2 == 1 && shipTop >= 80 && shipTop <= 220 && stone2Left <= 60 && stone2Left >= -80) {
+        lives--;
+    } else if (runStone3 == 1 && shipTop >= 150 && shipTop <= 290 && stone3Left <= 60 && stone3Left >= -80) {
+        lives--;
+    } else if (runStone4 == 1 && shipTop >= 220 && shipTop <= 360 && stone4Left <= 60 && stone4Left >= -80) {
+        lives--;
+    } else if (runStone5 == 1 && shipTop >= 290 && shipTop <= 430 && stone5Left <= 60 && stone5Left >= -80) {
+        lives--;
+    } else if (runStone6 == 1 && shipTop >= 360 && shipTop <= 500 && stone6Left <= 60 && stone6Left >= -80) {
+        lives--;
+    } else if (runStone7 == 1 && shipTop >= 430 && shipTop <= 570 && stone7Left <= 60 && stone7Left >= -80) {
+        lives--;
+    } else if (runStone8 == 1 && shipTop >= 500 && stone8Left <= 60 && stone8Left >= -80) {
+        lives--;
+    } else {
+        score++;
+    }
+
+    checkLives();
+    
+    document.getElementById("scoreSpan").innerHTML = score;
+    document.getElementById("livesSpan").innerHTML = lives;
+}
+
+function moveStone0() {
+    stone0.style.left = (parseInt(stone0.style.left) - 100) + "px";
+}
+
+function moveStone1() {
+    stone0.style.left = (parseInt(stone1.style.left) - 100) + "px";
+}
+
+function moveStone2() {
+    stone0.style.left = (parseInt(stone2.style.left) - 100) + "px";
+}
+
+function moveStone3() {
+    stone0.style.left = (parseInt(stone3.style.left) - 100) + "px";
+}
+
+function moveStone4() {
+    stone0.style.left = (parseInt(stone4.style.left) - 100) + "px";
+}
+
+function moveStone5() {
+    stone0.style.left = (parseInt(stone5.style.left) - 100) + "px";
+}
+
+function moveStone6() {
+    stone0.style.left = (parseInt(stone6.style.left) - 100) + "px";
+}
+
+function moveStone7() {
+    stone0.style.left = (parseInt(stone7.style.left) - 100) + "px";
+}
+
+function moveStone8() {
+    stone0.style.left = (parseInt(stone8.style.left) - 100) + "px";
+}
+
+function moveStone() {
+    if (runStone0 == 1) {
+        moveStone0();
+    } else if (runStone1 == 1) {
+        moveStone1();
+    } else if (runStone2 == 1) {
+        moveStone2();
+    } else if (runStone3 == 1) {
+        moveStone3();
+    } else if (runStone4 == 1) {
+        moveStone4();
+    } else if (runStone5 == 1) {
+        moveStone5();
+    } else if (runStone6 == 1) {
+        moveStone6();
+    } else if (runStone7 == 1) {
+        moveStone7();
+    } else if (runStone8 == 1) {
+        moveStone8();
+    } 
+
+    checkPosition();
+}
+
+function activateStone() {
+    if (stone0Left < -80) {
+        runStone0 = 0;
+        runStone1 = 1;
+    }
+
+    if (stone1Left < -80) {
+        runStone1 = 0;
+        runStone2 = 1;
+    }
+
+    if (stone2Left < -80) {
+        runStone2 = 0;
+        runStone3 = 1;
+    }
+
+    if (stone3Left < -80) {
+        runStone3 = 0;
+        runStone4 = 1;
+    }
+
+    if (stone4Left < -80) {
+        runStone4 = 0;
+        runStone5 = 1;
+    }
+
+    if (stone5Left < -80) {
+        runStone5 = 0;
+        runStone6 = 1;
+    }
+
+    if (stone6Left < -80) {
+        runStone6 = 0;
+        runStone7 = 1;
+    }
+
+    if (stone7Left < -80) {
+        runStone7 = 0;
+        runStone8 = 1;
+    }
+
+    if (stone8Left < -80) {
+        runStone0 = 0;
+        runStone1 = 1;
+    }
+
+    moveStone();
+}
+
+function upPressed() {
+    ship.style.top = (parseInt(ship.style.top) - 30) + "px";
+
+    activateStone();
+}
+
+function downPressed() {
+    ship.style.top = (parseInt(ship.style.top) + 30) + "px";
+
+    activateStone();
+}
+
+function reset() {
+    lives = 3;
+    score = 0;
+
+    document.getElementById("scoreSpan").innerHTML = score;
+    document.getElementById("livesSpan").innerHTML = lives;
+
+    ship.style.top = 270 + "px";
+    stone0.style.top = 0 + "px";
+    stone0.style.left = 920 + "px";
+    stone1.style.top = 70 + "px";
+    stone1.style.left = 920 + "px";
+    stone2.style.top = 140 + "px";
+    stone2.style.left = 920 + "px";
+    stone3.style.top = 210 + "px";
+    stone3.style.left = 920 + "px";
+    stone4.style.top = 280 + "px";
+    stone4.style.left = 920 + "px";
+    stone5.style.top = 350 + "px";
+    stone5.style.left = 920 + "px";
+    stone6.style.top = 420 + "px";
+    stone6.style.left = 920 + "px";
+    stone7.style.top = 490 + "px";
+    stone7.style.left = 920 + "px";
+    stone8.style.top = 560 + "px";
+    stone8.style.left = 920 + "px";
+}
